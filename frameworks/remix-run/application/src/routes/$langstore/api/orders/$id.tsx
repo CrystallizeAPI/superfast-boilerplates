3e1ea9fd0ba5b76d198455642bc3befbabaa747a
Dhairya <dhairiya96@gmail.com>
2023-03-10 09:47:04 +0100

    cleanup

--- frameworks/remix-run/application/src/routes/$langstore/api/orders/$id.tsx
+++ frameworks/remix-run/application/src/routes/$langstore/api/orders/$id.tsx
@@ -1,36 +1,31 @@
-import { createOrderFetcher } from "@crystallize/js-api-client";
-import {
-  CartWrapper,
-  handleOrderRequestPayload,
-} from "@crystallize/node-service-api-request-handlers";
-import { LoaderFunction } from "@remix-run/node";
-import { authenticatedUser } from "~/core/authentication.server";
-import { getContext } from "~/use-cases/http/utils";
-import { privateJson } from "~/core/bridge/privateJson.server";
-import { getStoreFront } from "~/use-cases/storefront.server";
-import { cartWrapperRepository } from "~/use-cases/services.server";
+import { createOrderFetcher } from '@crystallize/js-api-client';
+import { CartWrapper, handleOrderRequestPayload } from '@crystallize/node-service-api-request-handlers';
+import { LoaderFunction } from '@remix-run/node';
+import { authenticatedUser } from '~/core/authentication.server';
+import { getContext } from '~/use-cases/http/utils';
+import { privateJson } from '~/core/bridge/privateJson.server';
+import { getStoreFront } from '~/use-cases/storefront.server';
+import { cartWrapperRepository } from '~/use-cases/services.server';
 
 export const loader: LoaderFunction = async ({ request, params }) => {
-  const requestContext = getContext(request);
-  const { secret: storefront } = await getStoreFront(requestContext.host);
-  const auth: any = await authenticatedUser(request);
-  let cartId = requestContext.url.searchParams.get("cartId");
+    const requestContext = getContext(request);
+    const { secret: storefront } = await getStoreFront(requestContext.host);
+    const auth: any = await authenticatedUser(request);
+    let cartId = requestContext.url.searchParams.get('cartId');
 
-  let cartWrapper: CartWrapper | null | undefined = cartId
-    ? await cartWrapperRepository.find(cartId)
-    : null;
+    let cartWrapper: CartWrapper | null | undefined = cartId ? await cartWrapperRepository.find(cartId) : null;
 
-  return privateJson(
-    await handleOrderRequestPayload(null, {
-      fetcherById: createOrderFetcher(storefront.apiClient).byId,
-      user: auth.email,
-      orderId: params.id!,
-      checkIfOrderBelongsToUser: () => {
-        if (cartWrapper && cartWrapper?.extra?.orderId === params.id) {
-          return false;
-        }
-        return true;
-      },
-    })
-  );
+    return privateJson(
+        await handleOrderRequestPayload(null, {
+            fetcherById: createOrderFetcher(storefront.apiClient).byId,
+            user: auth.email,
+            orderId: params.id!,
+            checkIfOrderBelongsToUser: () => {
+                if (cartWrapper && cartWrapper?.extra?.orderId === params.id) {
+                    return false;
+                }
+                return true;
+            },
+        }),
+    );
 };
--- frameworks/remix-run/application/src/routes/$langstore/order/$id.tsx
+++ frameworks/remix-run/application/src/routes/$langstore/order/$id.tsx
@@ -1,38 +1,30 @@
-import { json, LoaderFunction, HeadersFunction } from "@remix-run/node";
-import { useLoaderData } from "@remix-run/react";
-import {
-  HttpCacheHeaderTaggerFromLoader,
-  StoreFrontAwaretHttpCacheHeaderTagger,
-} from "~/use-cases/http/cache";
-import { getContext } from "~/use-cases/http/utils";
-import { getStoreFront } from "~/use-cases/storefront.server";
-import Order from "~/ui/pages/Order";
+import { json, LoaderFunction, HeadersFunction } from '@remix-run/node';
+import { useLoaderData } from '@remix-run/react';
+import { HttpCacheHeaderTaggerFromLoader, StoreFrontAwaretHttpCacheHeaderTagger } from '~/use-cases/http/cache';
+import { getContext } from '~/use-cases/http/utils';
+import { getStoreFront } from '~/use-cases/storefront.server';
+import Order from '~/ui/pages/Order';
 
 export const headers: HeadersFunction = ({ loaderHeaders }) => {
-  return HttpCacheHeaderTaggerFromLoader(loaderHeaders).headers;
+    return HttpCacheHeaderTaggerFromLoader(loaderHeaders).headers;
 };
 
 export const loader: LoaderFunction = async ({ request, params }) => {
-  const requestContext = getContext(request);
-  const { shared } = await getStoreFront(requestContext.host);
+    const requestContext = getContext(request);
+    const { shared } = await getStoreFront(requestContext.host);
 
-  let cartId = requestContext.url.searchParams.get("cartId");
-  return json(
-    {
-      orderId: params.id,
-      cartId,
-    },
+    let cartId = requestContext.url.searchParams.get('cartId');
+    return json(
+        {
+            orderId: params.id,
+            cartId,
+        },
 
-    StoreFrontAwaretHttpCacheHeaderTagger(
-      "15s",
-      "1w",
-      ["order" + params.id],
-      shared.config.tenantIdentifier
-    )
-  );
+        StoreFrontAwaretHttpCacheHeaderTagger('15s', '1w', ['order' + params.id], shared.config.tenantIdentifier),
+    );
 };
 
 export default () => {
-  const { orderId, cartId } = useLoaderData();
-  return <Order id={orderId} cartId={cartId} />;
+    const { orderId, cartId } = useLoaderData();
+    return <Order id={orderId} cartId={cartId} />;
 };
