15483ec77b0928362d5904cb3cfbb96b11c2cd47
Dhairya <dhairiya96@gmail.com>
2023-03-10 09:44:46 +0100

    cleanup

--- shared/application/ui/pages/Order.tsx
+++ shared/application/ui/pages/Order.tsx
@@ -1,45 +1,45 @@
-import { useEffect, useState } from "react";
-import { useAppContext } from "../app-context/provider";
-import { ServiceAPI } from "~/use-cases/service-api";
-import { Price } from "../lib/pricing/pricing-component";
-import { ClientOnly } from "@crystallize/reactjs-hooks";
-import { OrderDisplay } from "../components/display-order";
+import { useEffect, useState } from 'react';
+import { useAppContext } from '../app-context/provider';
+import { ServiceAPI } from '~/use-cases/service-api';
+import { Price } from '../lib/pricing/pricing-component';
+import { ClientOnly } from '@crystallize/reactjs-hooks';
+import { OrderDisplay } from '../components/display-order';
 
 export default ({ id, cartId }: { id: string; cartId?: string }) => {
-  const [tryCount, setTryCount] = useState(0);
-  const [order, setOrder] = useState<any | null>(null);
-  const { state: contextState, _t } = useAppContext();
+    const [tryCount, setTryCount] = useState(0);
+    const [order, setOrder] = useState<any | null>(null);
+    const { state: contextState, _t } = useAppContext();
 
-  useEffect(() => {
-    let timeout: ReturnType<typeof setTimeout>;
-    (async () => {
-      try {
-        setOrder(
-          await ServiceAPI({
-            language: contextState.language,
-            serviceApiUrl: contextState.serviceApiUrl,
-          }).fetchOrder(id, cartId)
-        );
-      } catch (exception) {
-        timeout = setTimeout(() => {
-          setTryCount(tryCount + 1);
-        }, 500 * tryCount);
-      }
-    })();
-    return () => clearTimeout(timeout);
-  }, [id, tryCount]);
+    useEffect(() => {
+        let timeout: ReturnType<typeof setTimeout>;
+        (async () => {
+            try {
+                setOrder(
+                    await ServiceAPI({
+                        language: contextState.language,
+                        serviceApiUrl: contextState.serviceApiUrl,
+                    }).fetchOrder(id, cartId),
+                );
+            } catch (exception) {
+                timeout = setTimeout(() => {
+                    setTryCount(tryCount + 1);
+                }, 500 * tryCount);
+            }
+        })();
+        return () => clearTimeout(timeout);
+    }, [id, tryCount]);
 
-  return (
-    <div className="min-h-[70vh] items-center flex lg:w-content mx-auto w-full">
-      <ClientOnly>
-        {order ? (
-          <OrderDisplay order={order} />
-        ) : (
-          <div className="min-h-[70vh] items-center justify-center flex max-w-[500px] mx-auto">
-            <div className="loader" />
-          </div>
-        )}
-      </ClientOnly>
-    </div>
-  );
+    return (
+        <div className="min-h-[70vh] items-center flex lg:w-content mx-auto w-full">
+            <ClientOnly>
+                {order ? (
+                    <OrderDisplay order={order} />
+                ) : (
+                    <div className="min-h-[70vh] items-center justify-center flex max-w-[500px] mx-auto">
+                        <div className="loader" />
+                    </div>
+                )}
+            </ClientOnly>
+        </div>
+    );
 };
--- shared/application/ui/pages/OrderPlacedCart.tsx
+++ shared/application/ui/pages/OrderPlacedCart.tsx
@@ -1,48 +1,46 @@
-import { useEffect, useState } from "react";
-import { useLocalCart } from "../hooks/useLocalCart";
-import { ServiceAPI } from "~/use-cases/service-api";
-import { useAppContext } from "../app-context/provider";
-import useNavigate from "~/bridge/ui/useNavigate";
-import { OrderDisplay } from "../components/display-order";
+import { useEffect, useState } from 'react';
+import { useLocalCart } from '../hooks/useLocalCart';
+import { ServiceAPI } from '~/use-cases/service-api';
+import { useAppContext } from '../app-context/provider';
+import useNavigate from '~/bridge/ui/useNavigate';
+import { OrderDisplay } from '../components/display-order';
 
 export default ({ cartId }: { cartId: string }) => {
-  const { cart: localCart, empty } = useLocalCart();
-  const [tryCount, setTryCount] = useState(0);
-  const { state: appContextState, path } = useAppContext();
-  const navigate = useNavigate();
+    const { cart: localCart, empty } = useLocalCart();
+    const [tryCount, setTryCount] = useState(0);
+    const { state: appContextState, path } = useAppContext();
+    const navigate = useNavigate();
 
-  useEffect(() => {
-    let timeout: ReturnType<typeof setTimeout>;
-    (async () => {
-      try {
-        if (localCart.cartId === cartId) {
-          empty();
-        }
-        const cart = await ServiceAPI({
-          language: appContextState.language,
-          serviceApiUrl: appContextState.serviceApiUrl,
-        }).fetchCart(cartId);
-        if (cart?.extra?.orderId) {
-          if (cart?.customer?.isGuest === true) {
-            navigate(
-              path("/order/" + cart.extra.orderId + "?cartId=" + cart.cartId)
-            );
-          } else {
-            navigate(path("/order/" + cart.extra.orderId));
-          }
-        } else {
-          timeout = setTimeout(() => {
-            setTryCount(tryCount + 1);
-          }, 500 * tryCount);
-        }
-      } catch (exception) {
-        timeout = setTimeout(() => {
-          setTryCount(tryCount + 1);
-        }, 500 * tryCount);
-      }
-    })();
-    return () => clearTimeout(timeout);
-  }, [cartId, tryCount]);
+    useEffect(() => {
+        let timeout: ReturnType<typeof setTimeout>;
+        (async () => {
+            try {
+                if (localCart.cartId === cartId) {
+                    empty();
+                }
+                const cart = await ServiceAPI({
+                    language: appContextState.language,
+                    serviceApiUrl: appContextState.serviceApiUrl,
+                }).fetchCart(cartId);
+                if (cart?.extra?.orderId) {
+                    if (cart?.customer?.isGuest === true) {
+                        navigate(path('/order/' + cart.extra.orderId + '?cartId=' + cart.cartId));
+                    } else {
+                        navigate(path('/order/' + cart.extra.orderId));
+                    }
+                } else {
+                    timeout = setTimeout(() => {
+                        setTryCount(tryCount + 1);
+                    }, 500 * tryCount);
+                }
+            } catch (exception) {
+                timeout = setTimeout(() => {
+                    setTryCount(tryCount + 1);
+                }, 500 * tryCount);
+            }
+        })();
+        return () => clearTimeout(timeout);
+    }, [cartId, tryCount]);
 
-  return <div></div>;
+    return <div></div>;
 };
