de2748e151f9d34cd0bd06f67d2653a88c521a57
Veljko Simakovic <vsimakovic@gmail.com>
2023-04-11 16:53:02 +0200

    FIX: Added Voucher Tax calculation + Refactor
    Fixed the Tax Amount calculation after the Voucher has been applied and refactored the alterCartBasedOnDiscounts replacing it with funnctions

--- shared/application/use-cases/checkout/cart.ts
+++ shared/application/use-cases/checkout/cart.ts
@@ -1,224 +1,261 @@
 import {
-    Cart,
-    cartPayload,
-    CartPayload,
-    CartWrapper,
-    handleCartRequestPayload,
-    Price,
-} from '@crystallize/node-service-api-request-handlers';
-import { extractDisountLotFromItemsBasedOnXForYTopic, groupSavingsPerSkus } from './discount';
-import { validatePayload } from '../http/utils';
+  Cart,
+  CartItem,
+  cartPayload,
+  CartPayload,
+  CartWrapper,
+  handleCartRequestPayload,
+  Price,
+} from "@crystallize/node-service-api-request-handlers";
 import {
-    ClientInterface,
-    createProductHydrater,
-    Product,
-    ProductPriceVariant,
-    ProductVariant,
-} from '@crystallize/js-api-client';
-import { CrystallizeAPI } from '../crystallize/read';
-import { marketIdentifiersForUser } from '../marketIdentifiersForUser';
-import { Voucher } from '../contracts/Voucher';
-
-export async function alterCartBasedOnDiscounts(wrapper: CartWrapper): Promise<CartWrapper> {
-    const { cart, total } = wrapper.cart;
-    const lots = extractDisountLotFromItemsBasedOnXForYTopic(cart.items);
-    const savings = groupSavingsPerSkus(lots);
-    const existingDiscounts = total.discounts || [];
-    const voucher = wrapper.extra?.voucher as Voucher | undefined;
-    let newTotals: Price = {
-        gross: 0,
-        currency: total.currency,
-        net: 0,
-        taxAmount: 0,
-        discounts: [
-            {
-                amount: 0,
-                percent: 0,
-            },
-        ],
-    };
-
-    let voucherDiscount = {
-        amount: 0,
-        percent: 0,
-    };
-
-    const alteredItems = cart.items.map((item) => {
-        const saving = savings[item.variant.sku]?.quantity > 0 ? savings[item.variant.sku] : null;
-        const savingAmount = saving?.amount || 0;
-        const netAmount = item.price.net - savingAmount;
-        const taxAmount = (netAmount * (item.product?.vatType?.percent || 0)) / 100;
-        const grossAmount = netAmount + taxAmount;
-        const discount = {
-            amount: savingAmount,
-            percent: (savingAmount / (netAmount + savingAmount)) * 100,
-        };
-        newTotals.taxAmount += taxAmount;
-        newTotals.gross += grossAmount;
-        newTotals.net += netAmount;
-        newTotals.discounts![0].amount += discount.amount;
-        return {
-            ...item,
-            price: {
-                gross: grossAmount,
-                net: netAmount,
-                currency: item.price.currency,
-                taxAmount,
-                discounts: item.price.discounts ? [...item.price.discounts, discount] : [discount],
-            },
-        };
-    });
-
-    // calculate the New Discout Percentage
-    const newDiscounts = {
-        amount: newTotals.discounts![0].amount,
-        percent:
-            ((newTotals.net + newTotals.discounts![0].amount - newTotals.net) /
-                (newTotals.net + newTotals.discounts![0].amount)) *
-            100,
-    };
-
-    if (voucher) {
-        if (voucher.value.type === 'absolute') {
-            voucherDiscount = {
-                amount: voucher.value.number,
-                percent: (voucher.value.number / newTotals.gross) * 100,
-            };
-        }
-        if (voucher.value.type === 'percent') {
-            voucherDiscount = {
-                amount: (newTotals.gross * voucher.value.number) / 100,
-                percent: voucher.value.number,
-            };
-        }
-    }
-
-    //REDUCE THE TOTALS BY THE VOUCHER DISCOUNT
-    newTotals.net -= voucherDiscount?.amount;
-    newTotals.gross = newTotals.net + newTotals.taxAmount;
-
-    return {
-        ...wrapper,
-        cart: {
-            total: {
-                ...newTotals,
-                discounts: [...existingDiscounts, newDiscounts, voucherDiscount],
-            },
-            cart: {
-                items: alteredItems,
-            },
-        },
-        extra: {
-            ...wrapper.extra,
-            discounts: {
-                lots,
-                savings,
-            },
-        },
-    };
-}
+  extractDisountLotFromItemsBasedOnXForYTopic,
+  groupSavingsPerSkus,
+} from "./discount";
+import { validatePayload } from "../http/utils";
+import {
+  ClientInterface,
+  createProductHydrater,
+  Product,
+  ProductPriceVariant,
+  ProductVariant,
+} from "@crystallize/js-api-client";
+import { CrystallizeAPI } from "../crystallize/read";
+import { marketIdentifiersForUser } from "../marketIdentifiersForUser";
+import { Voucher } from "../contracts/Voucher";
 
 export async function hydrateCart(
-    apiClient: ClientInterface,
-    language: string,
-    body: any,
+  apiClient: ClientInterface,
+  language: string,
+  body: any
 ): Promise<[Cart, Voucher | undefined]> {
-    const api = CrystallizeAPI({
-        apiClient,
-        language,
-    });
-    const tenantConfig = await api.fetchTenantConfig(apiClient.config.tenantIdentifier);
-    const currency = tenantConfig.currency;
+  const api = CrystallizeAPI({
+    apiClient,
+    language,
+  });
+  const tenantConfig = await api.fetchTenantConfig(
+    apiClient.config.tenantIdentifier
+  );
+  const currency = tenantConfig.currency;
 
-    const marketIdentifiers = marketIdentifiersForUser(body.user);
+  const marketIdentifiers = marketIdentifiersForUser(body.user);
 
-    const pickStandardPrice = (
-        product: Product,
-        selectedVariant: ProductVariant,
-        currency: string,
-    ): ProductPriceVariant => {
-        // opinionated: if we have a `default` Price we take it
-        let variant = selectedVariant?.priceVariants?.find(
-            (price: ProductPriceVariant) =>
-                price?.identifier === 'default' &&
-                price?.currency?.toLocaleLowerCase() === currency.toLocaleLowerCase(),
-        );
+  const pickStandardPrice = (
+    product: Product,
+    selectedVariant: ProductVariant,
+    currency: string
+  ): ProductPriceVariant => {
+    // opinionated: if we have a `default` Price we take it
+    let variant = selectedVariant?.priceVariants?.find(
+      (price: ProductPriceVariant) =>
+        price?.identifier === "default" &&
+        price?.currency?.toLocaleLowerCase() === currency.toLocaleLowerCase()
+    );
 
-        //if we have a market price, we take that
-        if (variant?.priceFor?.price && variant?.priceFor?.price < variant?.price!) {
-            variant.price = variant?.priceFor?.price;
-        }
-
-        return (
-            variant ??
-            selectedVariant?.priceVariants?.[0] ?? {
-                price: selectedVariant?.price,
-                identifier: 'undefined',
-            }
-        );
-    };
-
-    const cart = await handleCartRequestPayload(validatePayload<CartPayload>(body, cartPayload), {
-        hydraterBySkus: createProductHydrater(apiClient, { useSyncApiForSKUs: false, marketIdentifiers }).bySkus,
-        currency,
-        perProduct: () => {
-            return {
-                topics: {
-                    name: true,
-                },
-            };
-        },
-        perVariant: () => {
-            return {
-                firstImage: {
-                    url: true,
-                    variants: {
-                        url: true,
-                        height: true,
-                        width: true,
-                    },
-                },
-            };
-        },
-        selectPriceVariant: (
-            product: Product,
-            selectedVariant: ProductVariant,
-            currency: string,
-        ): ProductPriceVariant => {
-            // opinionated: if we have a `Sales` Price we take it
-            const variant = selectedVariant?.priceVariants?.find(
-                (price: ProductPriceVariant) =>
-                    price?.identifier === 'sales' && price?.currency?.toLowerCase() === currency.toLocaleLowerCase(),
-            );
-            const standardPrice = pickStandardPrice(product, selectedVariant, currency);
-            if (!variant) {
-                return standardPrice;
-            }
-
-            if (!variant?.price) {
-                return standardPrice;
-            }
-
-            return variant;
-        },
-        basePriceVariant: (
-            product: Product,
-            selectedVariant: ProductVariant,
-            currency: string,
-        ): ProductPriceVariant => {
-            return pickStandardPrice(product, selectedVariant, currency);
-        },
-    });
-
-    let voucher: Voucher | undefined;
-    try {
-        voucher = (await api.fetchVoucher(body.extra?.voucher)) as Voucher;
-        if (voucher.isExpired) {
-            voucher = undefined;
-        }
-    } catch (exception) {
-        voucher = undefined;
+    //if we have a market price, we take that
+    if (
+      variant?.priceFor?.price &&
+      variant?.priceFor?.price < variant?.price!
+    ) {
+      variant.price = variant?.priceFor?.price;
     }
 
-    return [cart, voucher];
+    return (
+      variant ??
+      selectedVariant?.priceVariants?.[0] ?? {
+        price: selectedVariant?.price,
+        identifier: "undefined",
+      }
+    );
+  };
+
+  const cart = await handleCartRequestPayload(
+    validatePayload<CartPayload>(body, cartPayload),
+    {
+      hydraterBySkus: createProductHydrater(apiClient, {
+        useSyncApiForSKUs: false,
+        marketIdentifiers,
+      }).bySkus,
+      currency,
+      perProduct: () => {
+        return {
+          topics: {
+            name: true,
+          },
+        };
+      },
+      perVariant: () => {
+        return {
+          firstImage: {
+            url: true,
+            variants: {
+              url: true,
+              height: true,
+              width: true,
+            },
+          },
+        };
+      },
+      selectPriceVariant: (
+        product: Product,
+        selectedVariant: ProductVariant,
+        currency: string
+      ): ProductPriceVariant => {
+        // opinionated: if we have a `Sales` Price we take it
+        const variant = selectedVariant?.priceVariants?.find(
+          (price: ProductPriceVariant) =>
+            price?.identifier === "sales" &&
+            price?.currency?.toLowerCase() === currency.toLocaleLowerCase()
+        );
+        const standardPrice = pickStandardPrice(
+          product,
+          selectedVariant,
+          currency
+        );
+        if (!variant) {
+          return standardPrice;
+        }
+
+        if (!variant?.price) {
+          return standardPrice;
+        }
+
+        return variant;
+      },
+      basePriceVariant: (
+        product: Product,
+        selectedVariant: ProductVariant,
+        currency: string
+      ): ProductPriceVariant => {
+        return pickStandardPrice(product, selectedVariant, currency);
+      },
+    }
+  );
+
+  let voucher: Voucher | undefined;
+  try {
+    voucher = (await api.fetchVoucher(body.extra?.voucher)) as Voucher;
+    if (voucher.isExpired) {
+      voucher = undefined;
+    }
+  } catch (exception) {
+    voucher = undefined;
+  }
+
+  return [cart, voucher];
 }
+export const alterCartItemsAndCartTotalsBasedOnSavings = (
+  cartWrapper: CartWrapper,
+  savings: Savings
+): CartWrapper => {
+  let newTotals: Price = {
+    gross: 0,
+    currency: cartWrapper.cart.total.currency,
+    net: 0,
+    taxAmount: 0,
+    discounts: [
+      {
+        amount: 0,
+        percent: 0,
+      },
+    ],
+  };
+
+  const alteredItems: CartItem[] = cartWrapper.cart.cart.items.map(
+    (item: CartItem) => {
+      const saving =
+        savings[item.variant.sku]?.quantity > 0
+          ? savings[item.variant.sku]
+          : null;
+      const savingAmount = saving?.amount || 0;
+      const netAmount = item.price.net - savingAmount;
+      const taxAmount =
+        (netAmount * (item.product?.vatType?.percent || 0)) / 100;
+      const grossAmount = netAmount + taxAmount;
+      const discount = {
+        amount: savingAmount,
+        percent: (savingAmount / (netAmount + savingAmount)) * 100,
+      };
+      newTotals.taxAmount += taxAmount;
+      newTotals.gross += grossAmount;
+      newTotals.net += netAmount;
+
+      return {
+        ...item,
+        price: {
+          ...item.price,
+          gross: grossAmount,
+          net: netAmount,
+          currency: item.price.currency,
+          taxAmount,
+          discounts: item.price.discounts
+            ? [...item.price.discounts, discount]
+            : [discount],
+        },
+      };
+    }
+  );
+  return {
+    ...cartWrapper,
+    cart: {
+      ...cartWrapper.cart,
+      cart: {
+        ...cartWrapper.cart.cart,
+        items: alteredItems,
+      },
+      total: {
+        ...cartWrapper.cart.total,
+        ...newTotals,
+        discounts: cartWrapper.cart.total.discounts
+          ? [...cartWrapper.cart.total.discounts, ...newTotals?.discounts!]
+          : [...newTotals?.discounts!],
+      },
+    },
+  };
+};
+export const alterCartTotalsBasedOnVouchers = (wrapper: CartWrapper) => {
+  let voucherDiscount = {
+    amount: 0,
+    percent: 0,
+  };
+  if (wrapper?.extra?.voucher) {
+    if (wrapper.extra.voucher.value.type === "absolute") {
+      voucherDiscount = {
+        amount: wrapper.extra.voucher.value.number,
+        percent:
+          (wrapper.extra.voucher.value.number / wrapper.cart.total.net) * 100,
+      };
+    }
+    if (wrapper.extra.voucher.value.type === "percent") {
+      voucherDiscount = {
+        amount:
+          (wrapper.extra.voucher.value.number / 100) * wrapper.cart.total.net,
+        percent: wrapper.extra.voucher.value.number,
+      };
+    }
+  }
+  const currentCartTaxPercentage =
+    (wrapper.cart.total.taxAmount / wrapper.cart.total.net) * 100;
+
+  wrapper.cart.total.net -= voucherDiscount?.amount;
+
+  wrapper.cart.total.taxAmount = +(
+    (wrapper.cart.total.net * currentCartTaxPercentage) /
+    100
+  ).toFixed(2);
+
+  wrapper.cart.total.gross =
+    wrapper.cart.total.net + wrapper.cart.total.taxAmount;
+
+  return {
+    ...wrapper,
+    cart: {
+      ...wrapper.cart,
+      total: {
+        ...wrapper.cart.total,
+        discounts: wrapper.cart.total.discounts
+          ? [...wrapper.cart.total.discounts, voucherDiscount]
+          : [voucherDiscount],
+      },
+    },
+  };
+};
--- shared/application/use-cases/checkout/handleSaveCart.ts
+++ shared/application/use-cases/checkout/handleSaveCart.ts
@@ -1,38 +1,66 @@
-import { ClientInterface } from '@crystallize/js-api-client';
-import { CartWrapper } from '@crystallize/node-service-api-request-handlers';
-import { Voucher } from '../contracts/Voucher';
-import { RequestContext } from '../http/utils';
-import { alterCartBasedOnDiscounts, hydrateCart } from './cart';
-import { v4 as uuidv4 } from 'uuid';
-import { cartWrapperRepository } from '../services.server';
+import { ClientInterface } from "@crystallize/js-api-client";
+import { CartWrapper } from "@crystallize/node-service-api-request-handlers";
+import { Voucher } from "../contracts/Voucher";
+import { RequestContext } from "../http/utils";
+import {
+  alterCartItemsAndCartTotalsBasedOnSavings,
+  alterCartTotalsBasedOnVouchers,
+  hydrateCart,
+} from "./cart";
+import { v4 as uuidv4 } from "uuid";
+import { cartWrapperRepository } from "../services.server";
+import {
+  extractDisountLotFromItemsBasedOnXForYTopic,
+  groupSavingsPerSkus,
+} from "./discount";
 
-export default async (apiClient: ClientInterface, context: RequestContext, body: any) => {
-    const [cart, voucher] = await hydrateCart(apiClient, context.language, body);
-    return await handleAndSaveCart(cart, body.cartId as string, voucher);
+export default async (
+  apiClient: ClientInterface,
+  context: RequestContext,
+  body: any
+) => {
+  const [cart, voucher] = await hydrateCart(apiClient, context.language, body);
+  return await handleAndSaveCart(cart, body.cartId as string, voucher);
 };
 
-export async function handleAndSaveCart(cart: any, providedCartId: string, voucher?: Voucher): Promise<CartWrapper> {
-    let cartId = providedCartId;
-    let cartWrapper: null | CartWrapper = null;
-    let storedCartWrapper: null | CartWrapper = null;
-    if (cartId) {
-        storedCartWrapper = (await cartWrapperRepository.find(cartId)) || null;
-    } else {
-        cartId = uuidv4();
-    }
-    if (!storedCartWrapper) {
-        (cartWrapper = cartWrapperRepository.create(cart, cartId)), { voucher };
-    } else {
-        cartWrapper = { ...storedCartWrapper };
-        cartWrapper.cart = cart;
-        cartWrapper.extra.voucher = voucher;
-    }
+export async function handleAndSaveCart(
+  cart: any,
+  providedCartId: string,
+  voucher?: Voucher
+): Promise<CartWrapper> {
+  let cartId = providedCartId;
+  let cartWrapper: null | CartWrapper = null;
+  let storedCartWrapper: null | CartWrapper = null;
+  if (cartId) {
+    storedCartWrapper = (await cartWrapperRepository.find(cartId)) || null;
+  } else {
+    cartId = uuidv4();
+  }
+  if (!storedCartWrapper) {
+    cartWrapper = cartWrapperRepository.create(cart, cartId, { voucher });
+  } else {
+    cartWrapper = { ...storedCartWrapper };
+    cartWrapper.cart = cart;
+    cartWrapper.extra.voucher = voucher;
+  }
+  const lots = extractDisountLotFromItemsBasedOnXForYTopic(
+    cartWrapper.cart.cart.items
+  );
+  const savings = groupSavingsPerSkus(lots);
+  cartWrapper = alterCartItemsAndCartTotalsBasedOnSavings(cartWrapper, savings);
+  if (cartWrapper?.extra?.voucher)
+    cartWrapper = alterCartTotalsBasedOnVouchers(cartWrapper);
 
-    // handle discount
-    cartWrapper = await alterCartBasedOnDiscounts(cartWrapper);
+  cartWrapper.extra = {
+    ...cartWrapper.extra,
+    discounts: {
+      lots,
+      savings,
+    },
+  };
 
-    if (!cartWrapperRepository.save(cartWrapper)) {
-        return storedCartWrapper || cartWrapper;
-    }
-    return cartWrapper;
+  if (!cartWrapperRepository.save(cartWrapper)) {
+    return storedCartWrapper || cartWrapper;
+  }
+  return cartWrapper;
 }
