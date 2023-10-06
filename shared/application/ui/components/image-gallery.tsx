0b2d227f10d04d1c88093eb91a08ffb864102781
Dhairya <dhairiya96@gmail.com>
2023-10-06 15:42:41 +0200

    return empty object if no meta

--- shared/application/ui/components/image-gallery.tsx
+++ shared/application/ui/components/image-gallery.tsx
@@ -1,34 +1,27 @@
-import { Image } from "@crystallize/reactjs-components";
-import { Image as ImageType } from "../../use-cases/contracts/Image";
+import { Image } from '@crystallize/reactjs-components';
+import { Image as ImageType } from '../../use-cases/contracts/Image';
 
 export const ImageGallery: React.FC<{ images: ImageType[] }> = ({ images }) => {
-  const galleryHasOddNubmer = images?.length % 2 || false;
-  if (!images || images.length === 0) {
-    return null;
-  }
-  return (
-    <div className="frntr-img-gallery ">
-      {images.map((img, i) => {
-        if (img.variants.length === 0) return null;
-        const isPortraitImg = img.variants[0].height > img.variants[0].width;
-        return (
-          <div
-            key={i}
-            className={`${isPortraitImg ? "portrait" : "landscape"} frntr-img`}
-          >
-            <Image
-              {...img}
-              sizes={`${
-                i < 1 || (i === images?.length && !galleryHasOddNubmer)
-                  ? "50vw"
-                  : "33vw"
-              }`}
-              loading="lazy"
-              key={img.url}
-            />
-          </div>
-        );
-      })}
-    </div>
-  );
+    const galleryHasOddNubmer = images?.length % 2 || false;
+    if (!images || images.length === 0) {
+        return null;
+    }
+    return (
+        <div className="frntr-img-gallery ">
+            {images.map((img, i) => {
+                if (img.variants.length === 0) return null;
+                const isPortraitImg = img.variants[0].height > img.variants[0].width;
+                return (
+                    <div key={i} className={`${isPortraitImg ? 'portrait' : 'landscape'} frntr-img`}>
+                        <Image
+                            {...img}
+                            sizes={`${i < 1 || (i === images?.length && !galleryHasOddNubmer) ? '50vw' : '33vw'}`}
+                            loading="lazy"
+                            key={img.url}
+                        />
+                    </div>
+                );
+            })}
+        </div>
+    );
 };
